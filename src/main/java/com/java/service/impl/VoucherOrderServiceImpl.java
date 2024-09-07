1055d85a9fa9290c211d4aefb58928e5997011a4
sw961174314 <961174314@qq.com>
2024-09-07 10:12:14 +0800

    2024.9.7-03.优惠券秒杀-03.乐观锁解决超卖

--- src/main/java/com/java/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/java/service/impl/VoucherOrderServiceImpl.java
@@ -52,8 +52,8 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             // 库存不足
             return Result.fail("库存不足");
         }
-        // 5.扣减库存
-        boolean success = seckillVoucherService.update().setSql("stock = stock - 1").eq("voucher_id", voucherId).update();
+        // 5.扣减库存(添加乐观锁 用库存来防止超卖)
+        boolean success = seckillVoucherService.update().setSql("stock = stock - 1").eq("voucher_id", voucherId).gt("stock",0).update();
         if (!success) {
             // 扣减失败
             return Result.fail("库存不足");
