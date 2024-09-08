48925b35ffbc719e234ba634efdb402474237fda
sw961174314 <961174314@qq.com>
2024-09-08 09:34:37 +0800

    2024.9.8-04.分布式锁-03.Java调用Lua脚本改造分布式锁

--- src/main/java/com/java/utils/SimpleRedisLock.java
+++ src/main/java/com/java/utils/SimpleRedisLock.java
@@ -1,7 +1,10 @@
 package com.java.utils;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -12,6 +15,13 @@ public class SimpleRedisLock implements ILock {
     private static final String KEY_PREFIX = "lock:";
 
     private static final String ID_PREFIX = UUID.randomUUID().toString() + "-";
+
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     private StringRedisTemplate stringRedisTemplate;
 
@@ -40,14 +50,8 @@ public class SimpleRedisLock implements ILock {
      */
     @Override
     public void unlock() {
-        // 获取线程标识
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        // 获取锁中的标识
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        // 判断标识是否一致
-        if (threadId.equals(id)) {
-            // 释放锁
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
+        // 为解决判断标识和释放锁可能会分别触发导致释放错误的锁
+        // 调用lua脚本
+        stringRedisTemplate.execute(UNLOCK_SCRIPT, Collections.singletonList(KEY_PREFIX + name), ID_PREFIX + Thread.currentThread().getId());
     }
 }
